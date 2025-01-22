4a956c9fac28c9652015289bcdb75a3c82972469
Izana Khabouri <izana.khabouri@gmail.com>
2025-01-23 00:15:42 +0100

    feat: add model and service to manage artists

---
+++ src/app/services/artist.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from @angular/common/http; 
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArtistService {
+  readonly API_URL_ARTISTS = "http://localhost:8080/artists";
+
+  constructor(private http : HttpClient) { }
+
+  getAllArtists() : Observable<any[]> {
+    return this.http.get<any[]>(this.API_URL_ARTISTS);
+  }
+
+  getSpecificArtist(id: string) : Observable<any[]> {
+    return this.http.get<any[]>(this.API_URL_ARTISTS + "/" + id);
+  }
+
+  removeArtist(id : string): Observable<any> {
+    return this.http.delete<any>(this.API_URL_ARTISTS + "/" + id);
+  }
+
+  updateArtist(id: string, artist : any) : Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.put<any>(this.API_URL_ARTISTS + "/" + id, artist, { headers });
+  }
+
+  createArtist(artist : any) : Observable<any[]> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<any>(this.API_URL_ARTISTS, artist, { headers });
+  }
+
+}

197c22bac009a4a697963eec4a08d3246b746d09
Izana Khabouri <izana.khabouri@gmail.com>
2025-01-22 01:24:53 +0100

    feat: add model

---
+++ src/app/models/event.model.ts
@@ -0,0 +1,19 @@
+export class Event {
+    id: string = "";
+    label: string = "";
+    startDate: string = "";
+    endDate: string = "";
+    // artists: Artist[] = [];
+
+    copy(): Event {
+        return Object.assign(new Event(), this);
+    }
+
+    static fromJson(categoryJson: Event): Event {
+        return Object.assign(new Event(), categoryJson);
+    }
+
+    toJson() : Event {
+        return Object.assign({}, this);
+    } 
+}

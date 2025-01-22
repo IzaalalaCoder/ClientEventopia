ec2e5698bb559146649bcb68b6132d8dab494616
Izana Khabouri <izana.khabouri@gmail.com>
2025-01-23 00:54:03 +0100

    feat: update list artists component to really display list artist

--- src/app/components/list-artist/list-artist.component.ts
+++ src/app/components/list-artist/list-artist.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ArtistComponent } from '../artist/artist.component';
+import { ArtistService } from '../../services/artist.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list-artist',
-  imports: [],
+  imports: [CommonModule, ArtistComponent],
   templateUrl: './list-artist.component.html',
-  styleUrl: './list-artist.component.css'
+  styleUrl: './list-artist.component.css',
+  providers: [ArtistService]
 })
 export class ListArtistComponent {
+  @Input({
+    required: true,
+    alias: 'artists'
+  }) artists : any[] = [];
+  @Output() update = new EventEmitter<void>();
 
+  updateListArtists() {
+    this.update.emit();
+  }
 }

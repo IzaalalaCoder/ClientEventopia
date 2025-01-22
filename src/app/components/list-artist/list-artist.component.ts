ac4787fae467d509931c8d452f18b4e51d54c548
Izana Khabouri <izana.khabouri@gmail.com>
2025-01-23 00:50:15 +0100

    feat: add empty component to manage display list artists

---
+++ src/app/components/list-artist/list-artist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-artist',
+  imports: [],
+  templateUrl: './list-artist.component.html',
+  styleUrl: './list-artist.component.css'
+})
+export class ListArtistComponent {
+
+}

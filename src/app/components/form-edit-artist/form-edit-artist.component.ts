8257353b62af1f70fc1134501f2a24d7a3fbc747
Izana Khabouri <izana.khabouri@gmail.com>
2025-01-23 00:44:09 +0100

    feat: add component to edit exist artist

---
+++ src/app/components/form-edit-artist/form-edit-artist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-edit-artist',
+  imports: [],
+  templateUrl: './form-edit-artist.component.html',
+  styleUrl: './form-edit-artist.component.css'
+})
+export class FormEditArtistComponent {
+
+}

b3c09d6bb8cf31b5f81769518b9a1a66e76b100b
Izana Khabouri <izana.khabouri@gmail.com>
2025-01-23 00:57:13 +0100

    feat: add manage to form to create new artist

--- src/app/components/form-create-artist/form-create-artist.component.ts
+++ src/app/components/form-create-artist/form-create-artist.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ArtistService } from '../../services/artist.service';
+import { Artist } from '../../models/artist.model';
 
 @Component({
   selector: 'app-form-create-artist',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './form-create-artist.component.html',
   styleUrl: './form-create-artist.component.css'
 })
 export class FormCreateArtistComponent {
+  @Output() closeForm = new EventEmitter<void>();
+  @Output() addedA = new EventEmitter<void>();
 
+  artistForm: FormGroup;
+
+  constructor(private artistService: ArtistService) {
+    this.artistForm = new FormGroup({
+      label: new FormControl(''),
+    });
+  }
+
+  submit() {
+    if (this.artistForm.invalid) {
+      return;
+    }
+
+    const artist: Artist = new Artist();
+    artist.label = this.artistForm.value.label;
+
+    this.artistService.createArtist(artist).subscribe(() => {
+      this.addedA.emit();
+    });
+  }
+
+  closeEventForm() {
+    this.closeForm.emit();
+  }
 }

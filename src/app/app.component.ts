73b736cf1db6befc7a48702b0fb820a0c1e7e7c9
Izana Khabouri <izana.khabouri@gmail.com>
2025-01-23 00:43:17 +0100

    feat: add component to create new artist in menu bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EventService } from './services/event.service';
+import { ArtistService } from './services/artist.service';
 import { OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListEventComponent } from './components/list-event/list-event.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { Event } from './models/event.model';
+import { Artist } from './models/artist.model';
 
 @Component({
   selector: 'app-root',
@@ -17,8 +19,9 @@ import { Event } from './models/event.model';
 export class AppComponent implements OnInit {
   title = 'Eventopia';
   events: Event[] = [];
+  artists: Artist[] = [];
 
-  constructor(private eventService: EventService) {}
+  constructor(private eventService: EventService, private artistService: ArtistService) {}
 
   ngOnInit(): void {
     this.loadEvents();
@@ -27,6 +30,12 @@ export class AppComponent implements OnInit {
   loadEvents() {
     this.eventService.getAllEvents().subscribe((datas: any) => {
       this.events = datas.content;
+    });
+  }
+
+  loadArtists() {
+    this.artistService.getAllArtists().subscribe((datas: any) => {
+      this.artists = datas.content;
     });
   }
 }

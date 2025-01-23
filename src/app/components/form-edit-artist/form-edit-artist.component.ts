a6f1fb0cb3a6a4f84ba776195b82a3cf6f176a99
Izana Khabouri <izana.khabouri@gmail.com>
2025-01-23 01:09:48 +0100

    feat: improve behavior of form edit artist

--- src/app/components/form-edit-artist/form-edit-artist.component.ts
+++ src/app/components/form-edit-artist/form-edit-artist.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ArtistService } from '../../services/artist.service';
+import { Artist } from '../../models/artist.model';
 
 @Component({
   selector: 'app-form-edit-artist',
@@ -7,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrl: './form-edit-artist.component.css'
 })
 export class FormEditArtistComponent {
+  @Input() artist: any = [];
+  @Output() closeForm = new EventEmitter<void>();
+  @Output() updateA = new EventEmitter<void>();
+
+  artistForm: FormGroup = new FormGroup({
+    label: new FormControl("")
+  });
+
+  constructor(private artistService: ArtistService) { }
+
+  submit() {
+    if (this.artistForm.valid) {
+
+      const newArtist: Artist = new Artist();
+      newArtist.label = this.artistForm.value.label;
+
+      if (this.artist) {
+        this.artistService.updateArtist(this.artist.id, newArtist).subscribe(() => {
+          this.updateA.emit();
+          this.closeForm.emit();
+        });
+      }
+    }
+  }
+
+  closeArtistForm() {
+    this.closeForm.emit();
+  }
 
 }
